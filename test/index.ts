f2c5f0c8845bc2af7c09db3ac0230dae88618da2
Zoltan Kochan <zoltan.kochan@gmail.com>
2019-05-28 02:14:24 +0300

    test: add more cases

--- test/index.ts
+++ test/index.ts
@@ -2,17 +2,33 @@ import test = require('tape')
 import parseNpmTarballUrl from 'parse-npm-tarball-url'
 
 test('parse simple URL', t => {
-  const result = parseNpmTarballUrl('http://registry.npmjs.org/foo/-/foo-1.0.0.tgz')
+  const result = parseNpmTarballUrl('http://registry.yarnpkg.com/foo/-/foo-1.0.0.tgz')
 
   if (!result) {
     t.ok(result)
     return
   }
 
-  t.equal(result.host, 'registry.npmjs.org')
+  t.equal(result.host, 'registry.yarnpkg.com')
   t.ok(result.pkg)
   t.equal(result.pkg.name, 'foo')
   t.equal(result.pkg.version, '1.0.0')
+
+  t.end()
+})
+
+test('parse simple URL with prerelease version', t => {
+  const result = parseNpmTarballUrl('http://registry.yarnpkg.com/foo/-/foo-1.0.0-beta.0.tgz')
+
+  if (!result) {
+    t.ok(result)
+    return
+  }
+
+  t.equal(result.host, 'registry.yarnpkg.com')
+  t.ok(result.pkg)
+  t.equal(result.pkg.name, 'foo')
+  t.equal(result.pkg.version, '1.0.0-beta.0')
 
   t.end()
 })
@@ -45,6 +61,22 @@ test('parse URL of scoped package', t => {
   t.ok(result.pkg)
   t.equal(result.pkg.name, '@foo/bar')
   t.equal(result.pkg.version, '1.0.0')
+
+  t.end()
+})
+
+test('parse URL of scoped package with prerelease version', t => {
+  const result = parseNpmTarballUrl('http://registry.npmjs.org/@foo/bar/-/bar-1.0.0-beta.0.tgz')
+
+  if (!result) {
+    t.ok(result)
+    return
+  }
+
+  t.equal(result.host, 'registry.npmjs.org')
+  t.ok(result.pkg)
+  t.equal(result.pkg.name, '@foo/bar')
+  t.equal(result.pkg.version, '1.0.0-beta.0')
 
   t.end()
 })
